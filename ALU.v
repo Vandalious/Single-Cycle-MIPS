b41bb9933aeb83599cb899fe028a39807ee4e5d5
Vand <54948455+Vandalious@users.noreply.github.com>
2021-06-07 21:07:37 +0430

    Update ALU.v

--- ALU.v
+++ ALU.v
@@ -2,7 +2,7 @@ module ALU(in1, in2, ALUctrl, out, zero);
 
 	input signed [31:0] in1;
 	input signed [31:0] in2;
-	input [5:0] ALUctrl;
+	input [2:0] ALUctrl;
 
 	output reg signed [31:0] out;
 	output zero;
@@ -11,10 +11,10 @@ module ALU(in1, in2, ALUctrl, out, zero);
 
 	always @(*) begin
 		case (ALUctrl)
-			6'b100000: out <= in1 + in2;
-			6'b100010: out <= in1 - in2;
-			6'b100100: out <= in1 & in2;
-			6'b100101: out <= in1 | in2;
+			3'b010: out <= in1 + in2;
+			3'b110: out <= in1 - in2;
+			3'b000: out <= in1 & in2;
+			3'b001: out <= in1 | in2;
 		endcase
 	end
 
