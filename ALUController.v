fe213c38d206fb23f25a7bab84be944388790d14
Vand <54948455+Vandalious@users.noreply.github.com>
2021-06-07 22:10:48 +0430

    Update ALUController.v
    This unit is basically useless

--- ALUController.v
+++ ALUController.v
@@ -1,1 +1,11 @@
+module ALUcontroller(ALUop, func, ALUctrl); //pretty much useless in this implementation
 
+	input [2:0] ALUop;
+	input [4:0] func; //useless (just for the sake of the book's model)
+
+	output reg [2:0] ALUctrl;
+	
+	assign ALUctrl = ALUop;
+
+endmodule
+	
