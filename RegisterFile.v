053f348db2ec2d6db8e6a13b6946cadafc62af6d
Vand <54948455+Vandalious@users.noreply.github.com>
2021-06-09 01:11:27 +0430

    Update RegisterFile.v
    added some initial values to the first 7 registers

--- RegisterFile.v
+++ RegisterFile.v
@@ -15,6 +15,21 @@ module RegisterFile(reset, clk, ReadReg1, ReadReg2, WriteReg, WriteData, RegWrit
 	assign ReadData2 = Register[ReadReg2];
 
 	integer i;
+
+	initial begin
+		Register[0] <= 32'd0; //$0 = 0
+		Register[1] <= 32'd5; //$1 = 5
+		Register[2] <= 32'd654; //$2 = 654
+		Register[3] <= 32'd123987; //$3 = 123987
+		Register[4] <= -32'd53; //$4 = -53
+		Register[5] <= 32'd103; //$5 = 103
+		Register[6] <= 32'd2; //$6 = 2
+
+		for (i = 7; i < 32; i = i + 1) begin
+			Register[i] <= 32'd0; //rest of the Registers are set to 0
+		end
+	end
+
 	always @(posedge reset or posedge clk) begin
 		if (reset) begin
 			for (i = 1; i < 32; i = i + 1) begin
